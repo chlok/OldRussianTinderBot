ad6239a1cf979180129f85d089936914b9509d3a
Solomatin Dmitry <chlok@mail.ru>
2022-03-29 20:19:05 +0300

    Initial commit

---
+++ src/main/java/ru/liga/oldrussiantinderbot/OldRussianTinderBotApplication.java
@@ -0,0 +1,13 @@
+package ru.liga.oldrussiantinderbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldRussianTinderBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OldRussianTinderBotApplication.class, args);
+    }
+
+}
